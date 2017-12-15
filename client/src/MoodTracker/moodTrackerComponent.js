5ab2e60b08b2bac293bda168a03be128dce868e4
Laura Unaeze <l.unaeze5@gmail.com>
2017-12-15 14:22:53 -0700

    Beginning stages of Mood Tracker component.

---
+++ client/src/MoodTracker/moodTrackerComponent.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Calendar Class. Displays the calendar for users to click on
+class Calendar extends React.Component {
+  render() {
+    return(
+      <div>
+        <div className="calendar-row">
+        </div>
+        <div className="calendar-row">
+        </div>
+        <div className="calendar-row">
+        </div>
+        <div className="calendar-row">
+        </div>
+        <div className="calendar-row">
+        </div>
+        <div className="calendar-row">
+        </div>
+      </div>
+    )
+  }
+}
