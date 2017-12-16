b074ed4dd9edbbb78de128691bf619edb3b30d60
Unknown <drdito8@gmail.com>
2017-12-16 15:17:39 -0700

    Added react-router-dom dependnecy

--- server.js
+++ server.js
@@ -16,12 +16,11 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 //Allows us to use local styling from our public/assets folder.
 app.use(express.static('public'));
+
+app.use('/', index);
 app.use('/users', users);
 
-
-
-
 //Syncing with the database prior to listening on port 3001
-  app.listen(process.env.PORT || 3001, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.listen(process.env.PORT || 3001, function() {
+  console.log("App listening on PORT " + PORT);
+});
