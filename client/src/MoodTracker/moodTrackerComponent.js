1d63dbed0dee649008fea5e0a09a3f563c928451
Laura Unaeze <l.unaeze5@gmail.com>
2017-12-16 05:28:40 -0700

    Added new calendar row for days of week and more CSS styling.

--- client/src/MoodTracker/moodTrackerComponent.js
+++ client/src/MoodTracker/moodTrackerComponent.js
@@ -31,52 +31,63 @@ class Calendar extends React.Component {
   }
 
   render() {
+    // Hard coding calendar for January 2018.
+    // I need to find a way to generate calendars dynamically
     return(
       <div>
         <div className="calendar-row">
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(4)}
+          {this.renderSquare('S')}
+          {this.renderSquare('M')}
+          {this.renderSquare('T')}
+          {this.renderSquare('W')}
+          {this.renderSquare('Th')}
+          {this.renderSquare('F')}
+          {this.renderSquare('S')}
         </div>
         <div className="calendar-row">
           {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
           {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
         </div>
         <div className="calendar-row">
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(4)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+          {this.renderSquare(10)}
+          {this.renderSquare(11)}
+          {this.renderSquare(12)}
+          {this.renderSquare(13)}
         </div>
         <div className="calendar-row">
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(4)}
+          {this.renderSquare(14)}
+          {this.renderSquare(15)}
+          {this.renderSquare(16)}
+          {this.renderSquare(17)}
+          {this.renderSquare(18)}
+          {this.renderSquare(19)}
+          {this.renderSquare(20)}
         </div>
         <div className="calendar-row">
+          {this.renderSquare(21)}
+          {this.renderSquare(22)}
+          {this.renderSquare(23)}
+          {this.renderSquare(24)}
+          {this.renderSquare(25)}
+          {this.renderSquare(26)}
+          {this.renderSquare(27)}
+        </div>
+        <div className="calendar-row">
+          {this.renderSquare(28)}
+          {this.renderSquare(29)}
+          {this.renderSquare(30)}
+          {this.renderSquare(31)}
           {this.renderSquare(null)}
           {this.renderSquare(null)}
           {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(null)}
-          {this.renderSquare(4)}
         </div>
         <div className="calendar-row">
           {this.renderSquare(null)}
