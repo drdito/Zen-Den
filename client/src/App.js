8502d99f8eaf397a3aba1fde2be835a19ef11946
Trent Cooper <coop741@gmail.com>
2017-12-16 21:03:32 -0700

    Initial version of Blog page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./components/Containers/Dashboard";
 import Resources from "./components/Containers/Resources";
 import MoodTracker from "./components/Containers/MoodTracker";
 import Breathe from "./components/Containers/Breathe";
+import Blog from "./components/Containers/Blog";
 
 const App = () =>
   <Router>
@@ -14,6 +15,7 @@ const App = () =>
         <Route exact path="/resources" component={Resources} />
         <Route exact path="/moodtracker" component={MoodTracker} />
         <Route exact path="/breathe" component={Breathe} />
+        <Route exact path="/blog" component={Blog} />
       </Switch>
     </div>
   </Router>;
