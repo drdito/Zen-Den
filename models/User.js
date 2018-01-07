ae5b707444a141413542f465e4da421c3a227628
Unknown <drdito8@gmail.com>
2018-01-07 11:03:04 -0700

    New user data from front end successfully posts to database

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userDataSchema = new Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   firstName: { type: String, required: true },
-  lastname: { type: String, required: true }
+  lastName: { type: String, required: true }
 });
 
 const User = mongoose.model("users", userDataSchema);
