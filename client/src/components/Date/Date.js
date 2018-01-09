60392aa903cd81efa19bffecb801a3b70c13ec47
Laura Unaeze <l.unaeze5@gmail.com>
2018-01-08 18:19:11 -0700

    Rewrote onClick handlers and added inline styles object. Need to redo modal firing.

--- client/src/components/Date/Date.js
+++ client/src/components/Date/Date.js
@@ -1,5 +1,6 @@
 // Date Component. Used in Calendar Component for the mood tracker
 import React from 'react';
+import MoodPicker from '../MoodPicker';
 
 
 class ButtonSwitch extends React.Component {
@@ -8,27 +9,50 @@ class ButtonSwitch extends React.Component {
     this.handleTrueClick = this.handleTrueClick.bind(this);
     this.handleFalseClick = this.handleFalseClick.bind(this);
     this.state = {isTrue: true};
+    
   }
 
+  /* This one function is written to switch the state between true and false */
   handleTrueClick() {
-    this.setState({isTrue: true});
+    console.log("FUNCTION FIRE");
+    this.setState({isTrue: !this.state.isTrue});
   }
 
+  /* Original 'false' state function. Not being used at the moment. */
   handleFalseClick() {
     this.setState({isTrue: false});
   }
 
   render() {
     const isTrue = this.state.isTrue;
-    
+    console.log(this.state);
+    /*
     let button = null;
     if (isTrue) {
-      button = <button className="date" onClick={this.handleFalseClick} style={{backgroundColor: 'pink'}}>{this.props.value}</button>;
+      button = button className="date" onClick={this.handleTrueClick} style={{backgroundColor: 'pink'}}>{this.props.value}</button>;
     } else {
       button = <button className="date" onClick={this.handleTrueClick} style={{backgroundColor: 'yellow'}}>{this.props.value}</button>;
     }
+    */
 
-    return button;
+    const Styles = {
+      buttonStyle: {
+        // inline 'if else' statement
+        backgroundColor: this.state.isTrue ?  'pink' : 'yellow',
+      },
+      modalStyle: {
+        display: this.state.isTrue ? 'none' : '',
+      }
+    }
+
+    console.log(Styles.buttonStyle);
+
+    return (
+      <div>
+        <button className="date" onClick={this.handleTrueClick} style={Styles.buttonStyle}>{this.props.value}</button>
+        <MoodPicker style={Styles.modalStyle} /> {/*This part isn't working yet. We will have to fire the modal using the React way*/}
+      </div>
+    );
   }
 
 }
