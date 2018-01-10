fa46964888c90d0828e9a32c257d51287873b6bb
Laura Unaeze <l.unaeze5@gmail.com>
2018-01-09 21:21:01 -0700

    Added all the moods to the onClick events in ButtonSwitch under Date.js

--- client/src/components/Date/Date.js
+++ client/src/components/Date/Date.js
@@ -1,24 +1,24 @@
 // Date Component. Used in Calendar Component for the mood tracker
 import React from 'react';
-import { DropdownButton, MenuItem, ButtonToolbar } from 'react-bootstrap';
 
 class ButtonSwitch extends React.Component {
-
   constructor(props) {
     super(props);
     this.handleEmptyClick = this.handleEmptyClick.bind(this);
     this.handleContentClick = this.handleContentClick.bind(this);
     this.handleHappyClick = this.handleHappyClick.bind(this);
+    this.handleAnxiousClick = this.handleAnxiousClick.bind(this);
+    this.handleDepressedClick = this.handleDepressedClick.bind(this);
+    this.handleAngryClick = this.handleAngryClick.bind(this);
+    this.handleSuicidalClick = this.handleSuicidalClick.bind(this);
     this.state = {buttonValue: 0};
   }
 
-  /* This one function is written to switch the state between true and false */
   handleEmptyClick() {
     console.log("FUNCTION EMPTY FIRE");
     this.setState({buttonValue: 0});
   }
 
-  /* Original 'false' state function. Not being used at the moment. */
   handleContentClick() {
     console.log("FUNCTION CONTENT FIRE");
     this.setState({buttonValue: 1});
@@ -29,41 +29,63 @@ class ButtonSwitch extends React.Component {
     this.setState({buttonValue: 2});
   }
 
+  handleAnxiousClick() {
+    console.log("FUNCTION ANXIOUS FIRE");
+    this.setState({buttonValue: 3});
+  }
+
+  handleDepressedClick() {
+    console.log("FUNCTION DEPRESSED FIRE");
+    this.setState({buttonValue: 4});
+  }
+
+  handleAngryClick() {
+    console.log("FUNCTION ANGRY FIRE");
+    this.setState({buttonValue: 5});
+  }
+
+  handleSuicidalClick() {
+    console.log("FUNCTION SUICIDAL FIRE");
+    this.setState({buttonValue: 6});
+  }
 
   render() {
     const buttonValue = this.state.buttonValue;
     console.log(this.state);
     
     let button = null;
+    // no mood entered = default white
     if (buttonValue === 0) {
-      button = <button className="date" onClick={this.handleContentClick} style={{backgroundColor: 'pink'}}>{this.props.value}</button>;
+      button = <button className="date" onClick={this.handleContentClick}>{this.props.value}</button>;
     } 
+    // content mood = green
     else if (buttonValue === 1) {
-      button = <button className="date" onClick={this.handleHappyClick} style={{backgroundColor: 'yellow'}}>{this.props.value}</button>;
+      button = <button className="date" onClick={this.handleHappyClick} style={{backgroundColor: 'green', color: 'white'}}>{this.props.value}</button>;
     }
+    // happy mood = yellow
     else if (buttonValue === 2) {
-      button = <button className="date" onClick={this.handleEmptyClick} style={{backgroundColor: 'green'}}>{this.props.value}</button>;;
+      button = <button className="date" onClick={this.handleAnxiousClick} style={{backgroundColor: 'yellow'}}>{this.props.value}</button>;;
     }
-    
-    /*
-    const Styles = {
-      buttonStyle: {
-        // inline 'if else' statement
-        backgroundColor: this.state.isTrue ?  'pink' : 'yellow',
-      },
-      modalStyle: {
-        display: this.state.isTrue ? 'none' : '',
-      }
+    // anxious mood = orange
+    else if (buttonValue === 3) {
+      button = <button className="date" onClick={this.handleDepressedClick} style={{backgroundColor: 'orange'}}>{this.props.value}</button>;;
     }
-
-    console.log(Styles.buttonStyle); // test
-    */
+    // depressed mood = blue
+    else if (buttonValue === 4) {
+      button = <button className="date" onClick={this.handleAngryClick} style={{backgroundColor: 'blue', color: 'white'}}>{this.props.value}</button>;;
+    }
+    // angry mood = red
+    else if (buttonValue === 5) {
+      button = <button className="date" onClick={this.handleSuicidalClick} style={{backgroundColor: 'red', color: 'white'}}>{this.props.value}</button>;;
+    }
+    // suicidal mood = black
+    else if (buttonValue === 6) {
+      button = <button className="date" onClick={this.handleEmptyClick} style={{backgroundColor: 'black', color: 'white'}}>{this.props.value}</button>;;
+    }
 
     return button;
   }
 }
-
-
 
 // Date compontent. Functonal component.
 const Date = (props) => {
@@ -71,24 +93,7 @@ const Date = (props) => {
 
   return (
     <ButtonSwitch value={props.value} buttonsInstance/>
-
   );
 }
-
-
-
-/*
-// Original Date compontent. Functonal component.
-const Date = (props) => {
-  return (
-    <button className="date" data-toggle="modal" data-target="#exampleModal" >
-      {props.value}
-    </button>
-  );
-}
-*/
-
-
-
 
 export default Date;
