d594fdf4d8f4c3d0db54a2217ba3b9ad68e124a6
Laura Unaeze <l.unaeze5@gmail.com>
2018-01-09 18:06:13 -0700

    Committing before reverting to previous ButtonSwitch class component.

--- client/src/components/Date/Date.js
+++ client/src/components/Date/Date.js
@@ -3,7 +3,7 @@ import React from 'react';
 import MoodPicker from '../MoodPicker';
 
 
-class ButtonSwitch extends React.Component {
+class ToggleModal extends React.Component {
   constructor(props) {
     super(props);
     // this.handleTrueClick = this.handleTrueClick.bind(this);
@@ -31,8 +31,12 @@ class ButtonSwitch extends React.Component {
     });
   }
 
+  changeColor = () => {
+    
+  }
+
   render() {
-    const isTrue = this.state.isTrue;
+    // const isTrue = this.state.isTrue;
     console.log(this.state);
     /*
     let button = null;
@@ -43,6 +47,7 @@ class ButtonSwitch extends React.Component {
     }
     */
 
+    /*
     const Styles = {
       buttonStyle: {
         // inline 'if else' statement
@@ -53,14 +58,34 @@ class ButtonSwitch extends React.Component {
       }
     }
 
-    console.log(Styles.buttonStyle);
+    console.log(Styles.buttonStyle); // test
+    */
 
     return (
       <div>
-        <button className="date" onClick={this.toggleModal}>{this.props.value}</button>
+        <button className="date" onClick={this.toggleModal} style={{backgroundColor: this.props.children}}>{this.props.value}</button>
 
         <MoodPicker show={this.state.isOpen} onClose={this.toggleModal}>
-          `Here's some content for the modal`
+          <div className="modal-body">
+            <button /*onClick={saveMood}*/ type="button" className="moodColor" id="content" style={{backgroundColor: 'green'}}> {/*content*/}
+              Content
+            </button>
+            <button type="button" className="moodColor" id="happy" style={{backgroundColor: 'yellow'}}> {/*happy*/}
+              Happy
+            </button>
+            <button type="button" className="moodColor" id="anxious" style={{backgroundColor: 'orange'}}> {/*anxious*/}
+              Anxious
+            </button>
+            <button type="button" className="moodColor" id="depressed" style={{backgroundColor: 'blue', color: 'white'}}>   {/*depressed*/}
+              Depressed
+            </button>
+            <button type="button" className="moodColor" id="angry" style={{backgroundColor: 'red'}}>    {/*angry*/}
+              Angry
+            </button>
+            <button type="button" className="moodColor" id="suicidal" style={{backgroundColor: 'black', color: 'white'}}>  {/*suicidal*/}
+              Suicidal
+            </button>
+          </div>
         </MoodPicker>
       </div>
     );
@@ -68,20 +93,64 @@ class ButtonSwitch extends React.Component {
 
 }
 
+/*
+class ButtonSwitch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleTrueClick = this.handleTrueClick.bind(this);
+    this.handleFalseClick = this.handleFalseClick.bind(this);
+    this.state = {isTrue: true};    
+  }
+
+  // This one function is written to switch the state between true and false 
+  handleTrueClick() {
+    console.log("FUNCTION FIRE");
+    this.setState({isTrue: !this.state.isTrue});
+  }
+
+  render() {
+    const isTrue = this.state.isTrue;
+    console.log(this.state);
+    
+  //  let button = null;
+  //  if (isTrue) {
+  //    button = button className="date" onClick={this.handleTrueClick} style={{backgroundColor: 'pink'}}>{this.props.value}</button>;
+  //  } else {
+  //    button = <button className="date" onClick={this.handleTrueClick} style={{backgroundColor: 'yellow'}}>{this.props.value}</button>;
+  //  }
+    
+
+    
+    const Styles = {
+      buttonStyle: {
+        // inline 'if else' statement
+        backgroundColor: this.state.isTrue ?  'pink' : 'yellow',
+      },
+      modalStyle: {
+        display: this.state.isTrue ? 'none' : '',
+      }
+    }
+
+    console.log(Styles.buttonStyle); // test
+  }  
+
+}
+*/
 
 // Date compontent. Functonal component.
 const Date = (props) => {
   console.log(props.value);
 
   return (
-    <ButtonSwitch value={props.value} />
+    <ToggleModal value={props.value} />
 
   );
 }
 
 
+
 /*
-// Date compontent. Functonal component.
+// Original Date compontent. Functonal component.
 const Date = (props) => {
   return (
     <button className="date" data-toggle="modal" data-target="#exampleModal" >
