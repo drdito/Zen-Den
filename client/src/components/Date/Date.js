c7cc94c62ee58f836990f45ee18660007a79c751
Laura Unaeze <l.unaeze5@gmail.com>
2018-01-09 20:46:30 -0700

    Changed method for changing colors with onClick.

--- client/src/components/Date/Date.js
+++ client/src/components/Date/Date.js
@@ -1,38 +1,50 @@
 // Date Component. Used in Calendar Component for the mood tracker
 import React from 'react';
-
+import { DropdownButton, MenuItem, ButtonToolbar } from 'react-bootstrap';
 
 class ButtonSwitch extends React.Component {
+
   constructor(props) {
     super(props);
-    this.handleTrueClick = this.handleTrueClick.bind(this);
-    // this.handleFalseClick = this.handleFalseClick.bind(this);
-    this.state = {isTrue: true};
+    this.handleEmptyClick = this.handleEmptyClick.bind(this);
+    this.handleContentClick = this.handleContentClick.bind(this);
+    this.handleHappyClick = this.handleHappyClick.bind(this);
+    this.state = {buttonValue: 0};
   }
 
   /* This one function is written to switch the state between true and false */
-  handleTrueClick() {
-    console.log("FUNCTION FIRE");
-    this.setState({isTrue: !this.state.isTrue});
+  handleEmptyClick() {
+    console.log("FUNCTION EMPTY FIRE");
+    this.setState({buttonValue: 0});
   }
 
   /* Original 'false' state function. Not being used at the moment. */
-  handleFalseClick() {
-    this.setState({isTrue: false});
+  handleContentClick() {
+    console.log("FUNCTION CONTENT FIRE");
+    this.setState({buttonValue: 1});
   }
 
+  handleHappyClick() {
+    console.log("FUNCTION HAPPY FIRE");
+    this.setState({buttonValue: 2});
+  }
+
+
   render() {
-    const isTrue = this.state.isTrue;
+    const buttonValue = this.state.buttonValue;
     console.log(this.state);
     
     let button = null;
-    if (isTrue) {
-      button = <button className="date" onClick={this.handleTrueClick} style={{backgroundColor: 'pink'}}>{this.props.value}</button>;
-    } else {
-      button = <button className="date" onClick={this.handleTrueClick} style={{backgroundColor: 'yellow'}}>{this.props.value}</button>;
+    if (buttonValue === 0) {
+      button = <button className="date" onClick={this.handleContentClick} style={{backgroundColor: 'pink'}}>{this.props.value}</button>;
+    } 
+    else if (buttonValue === 1) {
+      button = <button className="date" onClick={this.handleHappyClick} style={{backgroundColor: 'yellow'}}>{this.props.value}</button>;
+    }
+    else if (buttonValue === 2) {
+      button = <button className="date" onClick={this.handleEmptyClick} style={{backgroundColor: 'green'}}>{this.props.value}</button>;;
     }
     
-
     /*
     const Styles = {
       buttonStyle: {
@@ -49,59 +61,16 @@ class ButtonSwitch extends React.Component {
 
     return button;
   }
-
 }
 
-/*
-class ButtonSwitch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleTrueClick = this.handleTrueClick.bind(this);
-    this.handleFalseClick = this.handleFalseClick.bind(this);
-    this.state = {isTrue: true};    
-  }
 
-  // This one function is written to switch the state between true and false 
-  handleTrueClick() {
-    console.log("FUNCTION FIRE");
-    this.setState({isTrue: !this.state.isTrue});
-  }
-
-  render() {
-    const isTrue = this.state.isTrue;
-    console.log(this.state);
-    
-  //  let button = null;
-  //  if (isTrue) {
-  //    button = button className="date" onClick={this.handleTrueClick} style={{backgroundColor: 'pink'}}>{this.props.value}</button>;
-  //  } else {
-  //    button = <button className="date" onClick={this.handleTrueClick} style={{backgroundColor: 'yellow'}}>{this.props.value}</button>;
-  //  }
-    
-
-    
-    const Styles = {
-      buttonStyle: {
-        // inline 'if else' statement
-        backgroundColor: this.state.isTrue ?  'pink' : 'yellow',
-      },
-      modalStyle: {
-        display: this.state.isTrue ? 'none' : '',
-      }
-    }
-
-    console.log(Styles.buttonStyle); // test
-  }  
-
-}
-*/
 
 // Date compontent. Functonal component.
 const Date = (props) => {
   console.log(props.value);
 
   return (
-    <ButtonSwitch value={props.value} />
+    <ButtonSwitch value={props.value} buttonsInstance/>
 
   );
 }
